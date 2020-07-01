a929b2f70830e2247e03cd30c983fb3bde70db7a
Florian Magalhaes <m.f.pro@outlook.com>
2020-07-01 15:45:21 +0200

    Route login fonctionnelle

--- controllers/user.js
+++ controllers/user.js
@@ -6,7 +6,7 @@ exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
         const user = new User({
-            email: req.body.email,
+            email: req.body.email.toLowerCase(),
             password: hash
         });
         user.save()
@@ -17,5 +17,22 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-    
+    User.findOne({ email: req.body.email.toLowerCase()})
+    .then(user => {
+        if(!user){
+           return res.status(401).json({error: "Utilisateur non trouvé !"});
+        }
+        bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+            if(!valid){
+            return res.status(401).json({error: "Mot de passe incorrect !"});
+            }
+            res.status(200).json({
+            userId: user._id,
+            token: "TOKEN"
+            });
+        })
+        .catch(e => res.status(500).json({e}));
+    })
+    .catch(e => res.status(500).json({e}));
 };
