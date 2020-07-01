24d43abae93145317c692a6ea896db5a3ff327b4
Florian Magalhaes <m.f.pro@outlook.com>
2020-07-01 17:37:31 +0200

    Ajout logique des routes get et post

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,21 +1,30 @@
 const Sauce = require("../models/Sause");
 
-exports.getAllSauces = (req, res, next){
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(e => res.status(400).json({e}));
+};
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id})
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+};
+
+exports.createSauce = (req, res, next) => {
+    const newSauce = new Sauce({
+        ...req.body
+    });
+    newSauce.save()
+    .then(() => res.status(201).json({message: "Sauce crée !"}))
+    .catch(e => res.status(400).json({e}));
+};
+
+exports.modifySauce = (req, res, next) => {
 
 };
 
-exports.getOneSauce = (req, res, next){
-
-};
-
-exports.createSauce = (req, res, next){
-
-};
-
-exports.modifySauce = (req, res, next){
-
-};
-
-exports.deleteSauce = (req, res, next){
+exports.deleteSauce = (req, res, next) => {
 
 };
