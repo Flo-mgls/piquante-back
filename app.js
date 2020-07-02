0b7213abb85794f2c22991e8090b0bebdffe06b6
Florian Magalhaes <m.f.pro@outlook.com>
2020-07-02 02:28:40 +0200

    Ajout d'Helmet

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
+const helmet = require("helmet");
 
 const userRoutes = require("./routes/user");
 const sauceRoutes = require("./routes/sauce");
@@ -12,6 +13,8 @@ mongoose.connect('mongodb+srv://f-magalhaes:P6Piquante@cluster0-1accl.mongodb.ne
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(helmet());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
